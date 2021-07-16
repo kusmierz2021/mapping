c4b24d95e7b2b4d8d7c2d7f0223aaf77b7764953
Rafał Kuśmierz <r.kusmierz@stud.elka.pw.edu.pl>
2021-07-16 15:08:38 +0200

    layer control panel added

--- map.py
+++ map.py
@@ -2,7 +2,7 @@ import folium
 import webbrowser
 
 html = """
-Country name:<br>
+Country name:
 <a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
 Population: %s 
 """
@@ -17,11 +17,12 @@ def get_color(name, named_countries):
 
 def show_map(data, named_countries, finish=False):
     map = folium.Map(location=[0, 0],
-                     zoom_start=1.95, tiles="Stamen Watercolor")
+                     zoom_start=1.95,
+                     tiles="Stamen Terrain" if finish else "Stamen Watercolor")
 
-    fg = folium.FeatureGroup(name="named countries")
-
-    fg.add_child(folium.GeoJson(
+    fgc = folium.FeatureGroup(name="countries information")
+    fgb = folium.FeatureGroup(name="boundaries")
+    fgb.add_child(folium.GeoJson(
         data=open('world.json', 'r', encoding='utf-8-sig').read(),
         style_function=lambda x: {'fillColor': 'green' if
                                   x['properties']['NAME'].title() in
@@ -34,14 +35,16 @@ def show_map(data, named_countries, finish=False):
                                                 country['properties']['POP2005']),
                                    width=200, height=100)
 
-            fg.add_child(folium.Marker(location=[country['properties']['LAT'],
-                                                 country['properties']['LON']],
-                                       popup=folium.Popup(iframe),
-                                       icon=folium.Icon(get_color(
+            fgc.add_child(folium.Marker(location=[country['properties']['LAT'],
+                                                  country['properties']['LON']],
+                                        popup=folium.Popup(iframe),
+                                        icon=folium.Icon(get_color(
                                                 country['properties']['NAME'],
                                                 named_countries))))
 
-    map.add_child(fg)
+    map.add_child(fgb)
+    map.add_child(fgc)
+    map.add_child(folium.LayerControl())
     map.save("map.html")
 
     webbrowser.open("map.html")
